b52606a8b54f6930379820ab9940af76b270d4dc
woshingmachine <134035634+woshingmachine@users.noreply.github.com>
2026-03-11 21:06:58 +0800

    Update CliSyntax.java to have new prefixes

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -7,9 +7,13 @@ public class CliSyntax {
 
     /* Prefix definitions */
     public static final Prefix PREFIX_NAME = new Prefix("n/");
-    public static final Prefix PREFIX_PHONE = new Prefix("p/");
+    public static final Prefix PREFIX_NUS_ID = new Prefix("id/");
+    public static final Prefix PREFIX_ROLE = new Prefix("role/");
+    public static final Prefix PREFIX_SOC_USERNAME = new Prefix("soc/");
+    public static final Prefix PREFIX_GITHUB_USERNAME = new Prefix("gh/");
     public static final Prefix PREFIX_EMAIL = new Prefix("e/");
-    public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
-    public static final Prefix PREFIX_TAG = new Prefix("t/");
+    public static final Prefix PREFIX_PHONE = new Prefix("p/");
+    public static final Prefix PREFIX_TUTORIAL_GROUP = new Prefix("t/");
+    public static final Prefix PREFIX_TAG = new Prefix("tag/");
 
 }
